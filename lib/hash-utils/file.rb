7684b92fd213624cc385c0fd86fb17fe281d60d0
Martin Kozák <martinkozak@martinkozak.net>
2011-02-25 19:18:01 +0100

    File#write and File#touch

---
+++ lib/hash-utils/file.rb
@@ -0,0 +1,41 @@
+# encoding: utf-8
+# (c) 2011 Martin Kozák (martinkozak@martinkozak.net)
+
+##
+# File extension.
+# @since 0.11.0
+#
+
+class File
+
+    ##
+    # Writes data to file and closes it in single call.
+    #
+    # @param [String] filepath path to file
+    # @param [String] data data for write
+    # @return [Integer] length of really written data
+    # @since 0.11.0
+    #
+    
+    def self.write(filepath, data = "")
+        len = nil
+        File.open(filepath, "w") do |io|
+            len = io.write(data)
+        end
+        return len
+    end
+    
+    ##
+    # Creates file with zero size and closes it. (In fact, +touch+ is
+    # usually used for it.)
+    #
+    # @param [String] file filepath path for create
+    # @since 0.11.0
+    #
+    
+    def self.touch(filepath)
+        File.open(filepath, "w").close()
+    end
+    
+end
+
