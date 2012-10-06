49ad49613874137dff604cd97f57314929c29d0e
Martin Kozák <martinkozak@martinkozak.net>
2012-10-06 15:37:15 +0200

    StringIO: fix for Rubinius

--- lib/hash-utils/stringio.rb
+++ lib/hash-utils/stringio.rb
@@ -7,19 +7,38 @@ require "hash-utils/object"
 # StringIO extension.
 #
 
-class StringIO < Data
+if Ruby::Engine != :rbx
+    class StringIO < Data
 
-    ##
-    # Indicates, object is IO, so +IO+ or +StringIO+ class.
-    #
-    # @return [Boolean] +true+ if yes, +false+ in otherwise
-    # @since 0.14.0
-    #
-    
-    if not self.__hash_utils_instance_respond_to? :io?
-        def io?
-            true
+        ##
+        # Indicates, object is IO, so +IO+ or +StringIO+ class.
+        #
+        # @return [Boolean] +true+ if yes, +false+ in otherwise
+        # @since 0.14.0
+        #
+        
+        if not self.__hash_utils_instance_respond_to? :io?
+            def io?
+                true
+            end
         end
+        
     end
-    
+else
+    class StringIO < Object
+
+        ##
+        # Indicates, object is IO, so +IO+ or +StringIO+ class.
+        #
+        # @return [Boolean] +true+ if yes, +false+ in otherwise
+        # @since 0.14.0
+        #
+        
+        if not self.__hash_utils_instance_respond_to? :io?
+            def io?
+                true
+            end
+        end
+        
+    end
 end
